1b18a0cbe1fa70d0494611199d4a6e4bb561b015
Roman <romantimilsina@gmail.com>
2023-01-07 14:06:44 +0545

    flashcard-part-done-and-header-part-left

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import FlashcardList from './FlashcardList.js'
+import './App.css'
+import axios from 'axios'
+import {v4 as uuidv4} from 'uuid'
+
 
 function App() {
+  const [flashcards, setFlashcards] = useState([])
+  console.log(flashcards)
+
+  useEffect(() => {
+    axios.get('https://opentdb.com/api.php?amount=10')
+    .then(res => {
+      
+      setFlashcards(res.data.results.map(perQuestion => {
+        
+        const question = decodeString(perQuestion.question)
+        const answer = perQuestion.correct_answer
+        const options = [...perQuestion.incorrect_answers, answer]
+        
+        
+        return {
+          id: uuidv4(),
+          question: question,
+          answer: answer,
+          options: options.sort(() => Math.random() - .5)
+        }
+       } ))
+      })
+      },[])
+      
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <FlashcardList flashcards = {flashcards} />
+      </div>
+    </>
   );
 }
 
 export default App;
+
+function decodeString(str){
+  const textArea = document.createElement('textarea');
+  textArea.innerHTML = str;
+  return textArea.value
+}
---
+++ src/Flashcard.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useRef, useState } from 'react'
+
+
+export default function Flashcard({flashcard}) {
+  const [flip, setFlip] = useState(false)
+  const [height, setHeight] = useState('initial')
+
+  const frontEl = useRef()
+  const backEl = useRef()
+
+  function setMaxHeight(){
+    const frontHeight = frontEl.current.getBoundingClientRect().height
+    const backHeight = backEl.current.getBoundingClientRect().height
+    setHeight(!flip ? frontHeight:backHeight)
+  }
+
+  useEffect(setMaxHeight, [flashcard.question, flashcard.answer,flashcard.option,flip])
+
+  useEffect(() => {
+    window.addEventListener('resize', setMaxHeight)
+    return () => window.removeEventListener('resize', setMaxHeight)
+  },[])
+
+  return (
+    <>
+    <div 
+    className={` card ${flip ? 'flip': ''}`}
+    onClick={() => setFlip(!flip)}
+    style={{height:height}}
+    >
+    
+    <div className='front' ref={frontEl}>
+      {flashcard.question}
+        <div className='front-options'>
+          {flashcard.options.map(option => {
+              return <div className='front-option' key={option}>{option}</div>
+           })}
+        </div>
+    </div>
+    <div className='back' ref={backEl}>{flashcard.answer}</div>
+   
+    </div>
+    
+      
+    </>
+  )
+}
